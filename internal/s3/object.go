4ddb08eee8a9fc05e3cce34768fc69eef09f19fe
Lorenz Kästle <lorenz.kaestle@netways.de>
2024-11-25 18:33:22 +0100

    Linter fix: make typecast explicit

--- internal/s3/object.go
+++ internal/s3/object.go
@@ -7,10 +7,16 @@ import (
 )
 
 func (v *V2Output) GetObjectOutput(size int64, status int, path string) (output string) {
+	var tmp_size uint64
+
+	if size >= 0 {
+		tmp_size = uint64(size)
+	}
+
 	output += fmt.Sprintf("   \\_[%s] %s: %s\n",
 		check.StatusText(status),
 		path,
-		convert.BytesIEC(size))
+		convert.BytesIEC(tmp_size))
 
 	return
 }
